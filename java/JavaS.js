1314ecccbc003119456dfdeee7bd21c7a3cf65ed
kaylarswebd <54419990+kaylarswebd@users.noreply.github.com>
2019-11-18 13:33:53 -0600

    Add files via upload

--- java/JavaS.js
+++ java/JavaS.js
@@ -32,10 +32,9 @@ $(document).ready(function () {
 
 });
 
-//Get the button
+//back to top button
 var mybutton = document.getElementById("myBtn3");
 
-// When the user scrolls down 20px from the top of the document, show the button
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
@@ -46,7 +45,6 @@ function scrollFunction() {
   }
 }
 
-// When the user clicks on the button, scroll to the top of the document
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
